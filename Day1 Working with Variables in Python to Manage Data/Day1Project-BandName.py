f0949e735b1875e27aea5c29af23c1656bcab201
argimboldo <gianmarcoannese@gmail.com>
2023-01-25 15:27:10 +0100

    Update Day1Project-BandName.py

--- Day1 Working with Variables in Python to Manage Data/Day1Project-BandName.py
+++ Day1 Working with Variables in Python to Manage Data/Day1Project-BandName.py
@@ -1,1 +1,12 @@
+#Day1 Project - Band name
+#1. Create a greeting for your program.
+print("Hi, welcome to the Band name generator!")
 
+#2. Ask the user for the city that they grew up in.
+city = input("What's the city where you grew up?")
+
+#3. Ask the user for the name of a pet.
+pet_name = input("What is your pet name?")
+
+#4. Combine the name of their city and pet and show them their band name
+print("Your band name could be: \n"+city+" "+pet_name)
