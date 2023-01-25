42ef197787be4c6398d3e37d9fd7ee74612a3838
argimboldo <gianmarcoannese@gmail.com>
2023-01-25 16:55:21 +0100

    Update Day2Project-TipCalculator.py

--- Day2 Understanding Data Types and How to Manipulate Strings/Day2Project-TipCalculator.py
+++ Day2 Understanding Data Types and How to Manipulate Strings/Day2Project-TipCalculator.py
@@ -1,1 +1,17 @@
+# Tip Calculator 
+print  ("Welcome to the Tip Calculator!")
 
+# Input
+bill = input("What was the total bill?: $")
+perc = input("What percentage tip would you like to give? 10, 12 or 15%? ")
+ppl  = input("How many people to split the bill?: ")
+
+# Calc
+bill_num = float(bill)
+perc_num = int(perc)
+ppl_num  = int(ppl)
+
+perc_appl = round((bill_num/100)*perc_num,2)
+perc_per_ppl = round(perc_appl/ppl_num,2)
+
+print (f"If you want to give {perc_num}% bill on {bill_num}$, everyone of you have to pay {perc_per_ppl}$")
